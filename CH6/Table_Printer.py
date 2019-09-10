cce1773f337a0199e0d945419b51f1fde35a4295
Clockknight <tylerf2wang@gmail.com>
2019-09-09 17:14:14 -0700

    Update
    -Completed CH7
    -Made file outlines for the other chapters

--- CH6/Table_Printer.py
+++ CH6/Table_Printer.py
@@ -1,14 +1,44 @@
 width = 0
-iteration = 0
+tracker = 0
+lineString = ''
 tableData = [['apples', 'oranges', 'cherries', 'banana'],
              ['Alice', 'Bob', 'Carol', 'David'],
              ['dogs', 'cats', 'moose', 'goose']]
 
-while iteration < len(tableData):
-    if len(tableData[iteration]) > width:
-        width = len(tableData[iteration])
+x = len(tableData)
+#print(x)
+
+iteration = 0
+while iteration < x:
+    y = len(tableData[iteration])
+    iterationAlternate = 0
+    #print(iterationAlternate)
+    #print(y)
+    while iterationAlternate < y:
+        #print(str(tableData[iteration][iterationAlternate]) + ' ' + str(len(tableData[iteration][iterationAlternate])))
+        if width < len(tableData[iteration][iterationAlternate]):
+            width = len(tableData[iteration][iterationAlternate])
+        iterationAlternate += 1
     iteration += 1
 
 width += 3
+#print(width)
 
-print(width)
+#The program has taken the longest string length, and stored it in width
+
+iteration = 0
+while iteration < x:
+    y = len(tableData[iteration])
+    iterationAlternate = 0
+    while iterationAlternate < y:
+        #print(tableData[iteration][iterationAlternate])
+        tableData[iteration][iterationAlternate] = str(tableData[iteration][iterationAlternate]).rjust(width)
+        #print(tableData[iteration][iterationAlternate])
+        lineString += tableData[iteration][iterationAlternate]
+        tracker += 1
+        if tracker == 3:
+            print(lineString)
+            tracker = 0
+            lineString = ''
+        iterationAlternate += 1
+    iteration += 1
