dae40429d838eecef213af42d37c3ce14999f164
Clockknight <tylerf2wang@gmail.com>
2019-11-28 18:48:33 -0800

    Opens incorrect tabs
    Cannot find a tags inside of tags with the r class

---
+++ CH11/Notes/lucky.py
@@ -0,0 +1,27 @@
+#! python3
+#   lucky.py - Opens several Google search results.
+
+import requests, sys, webbrowser, bs4, logging
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+#logging.disable(debug)
+
+print('Googling...') #display text while downloading the google page
+url = ('http://google.com/search?q=' + 'whale')
+res = requests.get(url)
+res.raise_for_status()
+#logging.debug('res is: ' + res.text)
+
+# TODO: Retrieve top search result links.
+soup = bs4.BeautifulSoup(res.text)
+#logging.debug('soup is: ' + str(soup)) 
+
+# TODO: Open a browser tab for each result.
+linkElems = soup.select('a')
+#logging.debug('linkElems = ' + str(linkElems))
+numOpen = min(5, len(linkElems))
+for i in range(numOpen):
+    webbrowser.open('http://google.com' + linkElems[i].get('href'))
+
+
+#When done, change hardcoded whale back into ' '.join(sys.argv[1:]))
