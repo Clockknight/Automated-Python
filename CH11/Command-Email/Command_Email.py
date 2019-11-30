6c957b2a0e310c95df90a21ec33e55bbd1aef66f
Clockknight <tylerf2wang@gmail.com>
2019-11-30 08:53:34 -0800

    Code opens to correct link

--- CH11/Command-Email/Command_Email.py
+++ CH11/Command-Email/Command_Email.py
@@ -6,7 +6,19 @@ from selenium import webdriver
 logging.basicConfig(level=logging.DEBUG, format = '%(levelname)s - %(message)s')
 #logging.disable(Debug)
 
+#Collect variables to send
 target = input('Please input target email:\n')
 logging.debug(target)
 message = input('Please input intended message:\n')
 logging.debug(message)
+
+#Open the browser to hardcoded url
+browser = webdriver.Chrome()
+browser.get("https://www.mail.com/")
+
+#Find and click the button
+loginButton = browser.find_element_by_id('login-button')
+loginButton.click()
+
+#Find and fillout the login & password areas
+loginForm = browser.find_element_by_name('username')
