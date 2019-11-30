65fec864d08333ff96f222e1301a36ae8278a15a
Clockknight <tylerf2wang@gmail.com>
2019-11-29 17:22:32 -0800

    successfully collects inputs from user

--- CH11/Command-Email/Command_Email.py
+++ CH11/Command-Email/Command_Email.py
@@ -1,3 +1,12 @@
 import os
+import logging
+import selenium
+from selenium import webdriver
 
-input('Please input text')
+logging.basicConfig(level=logging.DEBUG, format = '%(levelname)s - %(message)s')
+#logging.disable(Debug)
+
+target = input('Please input target email:\n')
+logging.debug(target)
+message = input('Please input intended message:\n')
+logging.debug(message)
