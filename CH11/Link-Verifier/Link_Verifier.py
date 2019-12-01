2d8da79f2ce825665c14c21fc5deb315a2a54101
Clockknight <tylerf2wang@gmail.com>
2019-11-30 21:32:17 -0800

    Update
    Code now takes yes or no answer, but still doesn't do anything with it.

--- CH11/Link-Verifier/Link_Verifier.py
+++ CH11/Link-Verifier/Link_Verifier.py
@@ -2,12 +2,21 @@ import os
 import bs4
 import requests
 
-#url = input('Please choose a website to try and download:\n')
+#Gather url and send it to res to check if it's functional
+#url = input('Please choose a website to try and download (don't forget to put http:// at the start):\n')
 url = 'http://google.com'
 res = requests. get(url)
 res.raise_for_status()
 
+#Gather <a> tags from the site
 soup = bs4.BeautifulSoup(res.text, features="lxml")
 linkList = soup.find_all('a')
 soupLen = len(linkList)
-print("Found " + str(soupLen) + " links in the document")
+
+#Confirm download
+print("Found " + str(soupLen) + " links at " + url + ".\n")
+answer = input("Do you want to continue? Yes/No\n")
+while (answer != 'Yes' and 'No' ):
+    answer = input("\nDo you want to continue? Yes/No\n")
+
+#
