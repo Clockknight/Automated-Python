33ff2d1ba9dd5264a842c928ddf69929823f6f1d
Clockknight <tylerf2wang@gmail.com>
2019-12-08 17:03:50 -0800

    Temporary update, code doesn't download images yet

--- CH11/Image-Downloader/Image_Downloader.py
+++ CH11/Image-Downloader/Image_Downloader.py
@@ -8,8 +8,10 @@ from selenium.webdriver.common.keys import Keys
 import requests
 
 #Initialize Webdriver to browse imgur
-#query = 'http://imgur.com/search?q=' + input('Please input a term to search imgur for:\n')
-queryUrl = 'http://imgur.com/search?q=meme' #placeholder
+#query =  input('Please input a term to search imgur for:\n')
+query = 'meme' #placeholder
+queryUrl = 'http://imgur.com/search?q=' + query
+os.makedirs(str(query), exist_ok=True)
 
 browser = webdriver.Chrome()
 browser.get(queryUrl)
@@ -22,36 +24,52 @@ results = len(images)
 #Skips to end if no images are found.
 if results == 0:
     print('No results found.\nClosing program...')
+    sys.exit(0)
 
 #Download confirmation
+print('Found ' + str(results) + ' images.')
+answer = 'Yes' #answer = input('Do you wish to download?\nYes/No\n')
+while (answer != 'Yes' and answer != 'No'):
+    answer = input('\nInvalid answer, this program only reads either Yes or No.\nDo you wish to download?\nYes/No\n')
+if answer == 'No':
+    print('Okay. Shutting down...')
 else:
-    print('Found ' + str(results) + ' images.')
-    answer = input('Do you wish to download?\nYes/No\n')
-    while (answer != 'Yes' and answer != 'No'):
-        answer = input('\nInvalid answer, this program only reads either Yes or No.\nDo you wish to download?\nYes/No\n')
-    if answer == 'No':
-        print('Okay. Shutting down...')
-    else:
-        for i in range(results):
-            print('Downloading image #' + str(i+1) + '...')
+    for i in range(results):
+        print('Downloading image #' + str(i+1) + '...')
+        try:
+            #Go to the ith image
+            currentImage = images[i]
+            currentImage.send_keys(Keys.ENTER)
+            imageUrl = browser.current_url
+            browser.get(queryUrl)
+            images = browser.find_elements_by_class_name('image-list-link')
+
+            #Use bs4 to scan the result page
+            res = requests.get(imageUrl)
+            res.raise_for_status()
+            soup = bs4.BeautifulSoup(res.text, features="lxml")
+
+            #find images with BeautifulSoup
+            source = soup.find('link', {'rel': 'image_src', 'href': True}).get('href')
+            srcRes = requests.get(source)
+
             try:
-                #Go to the ith image
-                currentImage = images[i]
-                currentImage.send_keys(Keys.ENTER)
-                imageUrl = browser.current_url
-                browser.get(queryUrl)
+                srcRes.raise_for_status()
 
-                #Use bs4 to scan the image
-                res = requests.get(imageUrl)
-                res.raise_for_status()
-                soup = bs4.BeautifulSoup(res.text)
-                #find image with BeautifulSoup
                 #download image
-            except:
-                print('Bad download. Skipping...')
+                imageDownload = open(os.path.join('results', os.path.basename('Result #' + str(1+i)), 'wb'))
+                for chunk in srcRes.iter_content(100000):
+                    imageDownload.write(chunk)
+                imageDownload.close()
 
+            except Exception as exc:
+                print('There was a Problem: %s\nSkipping...' % (exc))
+
+
+
+        except Exception as exc:
+            print('There was a Problem: %s\nSkipping...' % (exc))
 
 
 #Close the program afterwards
 browser.quit()
-sys.exit(0)
