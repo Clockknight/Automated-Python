cdd53057f404ead46531ad56278273f28b4a9386
Clockknight <tylerf2wang@gmail.com>
2019-12-14 10:33:06 -0800

    Temporary Update

---
+++ CH12/Notes/readCensusExcel.py
@@ -0,0 +1,19 @@
+import openpyxl, pprint
+
+print('Opening workbook...')
+wb = openpyxl.load_workbook('censuspopdata.xlsx')
+sheet = wb.get_sheet_by_name('Populatio by census tract')
+countyData = {}
+
+#TODO: Fill in countyData with each county's population and tracts.
+print('Reading rows...')
+for row in range(2, sheet.max_row + 1):
+    #Each row in the spreadsheet has data for one census tract.
+    state = sheet['B' + str(row)].value
+    county = sheet['C' + str(row)].value
+    pop = sheet['D' + str(row)].value
+
+    countyData.setdefault(state, {})
+    countyData[state].setdefault(county, {'tracts': 0, 'pop': 0})
+
+#TODO: Open a new text file and write the contents of countyData to it.
