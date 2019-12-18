e0a153be8465a137b870493a8777d4290a74f0e5
Clockknight <Tylerf2wang@gmail.com>
2019-12-17 17:56:00 -0800

    textToSpreadsheet progress

--- CH12/Text-to-Spreadsheet/textToSpreadsheet.py
+++ CH12/Text-to-Spreadsheet/textToSpreadsheet.py
@@ -2,3 +2,21 @@ import os
 import sys
 import openpyxl
 from openpyxl import workbook
+
+#On startup, ask user for files. Answer "EXIT PROGRAM" once you have selected the files you want to.
+#A few outcomes:
+while True:
+    #File found, process it and keep track of how many files have been processed
+    if input
+        #Use readlines() to get a list of strings
+
+        #Make values = the appropriate index in the string thru a for loop
+
+
+    #EXIT PROGRAM, leave and save the spreadsheet
+    elif input == 'EXIT PROGRAM':
+        print('Thank you, closing program...')
+        sys.exit()
+    #File not found, try again
+    elif input :
+        print('File not found. Try again.')
