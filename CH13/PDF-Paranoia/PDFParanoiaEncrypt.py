178afd01e4168adae38d311dcf05a34f6aa69452
Clockknight <tylerf2wang@gmail.com>
2020-01-01 09:23:20 -0800

    Implemented Encrypting

---
+++ CH13/PDF-Paranoia/PDFParanoiaEncrypt.py
@@ -0,0 +1,37 @@
+import os
+import sys
+import PyPDF2
+
+fileList = [] #Initialize array
+
+
+#Scan for pdf files to encrypt
+for root, dirs, files in os.walk('.', topdown = False):
+    for name in files:
+        if name[-4:] == '.pdf':
+            fileList.append(os.path.join(root, name))
+
+for file in fileList:
+    print('File found at:\n\t' + file + '\nEncrypting file...\n\n')
+
+    #Encrypt all the files
+    pdfFile = open(file, 'rb')
+    pdfReader = PyPDF2.PdfFileReader(pdfFile)
+    pdfWriter = PyPDF2.PdfFileWriter()
+
+    #Add '_encrypted to pdf'
+    newFilename = file[:-4] + '_encrypted.pdf'
+
+    #Copy the original pdf to writer, page by page
+    for pageNum in range(pdfReader.numPages):
+        pdfWriter.addPage(pdfReader.getPage(pageNum))
+
+    #Encrypt the copy
+    pdfWriter.encrypt('swordwish')
+    resultPdf = open(newFilename, 'wb')
+    pdfWriter.write(resultPdf)
+    resultPdf.close()
+
+    #Decrypt the new file
+
+    #Delete the file
