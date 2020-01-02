534352ad55dd64e8c8350b7fb934f99bd7b7fee8
Clockknight <tylerf2wang@gmail.com>
2020-01-01 20:27:53 -0800

    Brute Force update

--- CH13/Brute-Force/Brute_Force.py
+++ CH13/Brute-Force/Brute_Force.py
@@ -2,23 +2,26 @@ import os
 import sys
 import PyPDF2
 
-success = 0
+processFailure = True
+
 lockedFile = PyPDF2.PdfFileReader(open('encryptedminutes.pdf', 'rb'))
+
+
 
 #Read from dictionary.txt
 dictFile = open(dictionary.txt, 'r')
 
 for line in dictFile: #For looping through passwords
-    password = dictionary[word][:-2]    #Each line is [:-2], to account for the \n at the end
-
+    password = line[:-2]    #Each line is [:-2], to account for the \n at the end
+    print('Trying %s as a password...' % password)
     #Break if decrypt returns 1
-    if encryptedReader.decrypt(password) == 0:
+    if encryptedReader.decrypt(password) == 1:
         print('Success! The password was %s' % password)
-        success = 1
+        processFailure = False
         break
 
     #Don't do anything if it returns 0
 
 #Fail case
-if success == 0:
+if processFailure:
     print('The password was not found in the list given.')
