d8e367b7907d3aa5623cd729b1fb5aac2f54b1e0
Clockknight <tylerf2wang@gmail.com>
2020-02-03 19:11:52 -0800

    Update

--- CH13/PDF-Paranoia/PDF_ParanoiaDecrypt.py
+++ CH13/PDF-Paranoia/PDF_ParanoiaDecrypt.py
@@ -1,3 +1,18 @@
 import os
 import sys
 import PyPDF2
+
+#Initializing Variables
+fileList = []
+
+#os.walk to find all .pdf files
+for root, dirs, files in os.walk('.', topdown = False):
+    for name in files:
+        if name[-4:] == '.pdf':
+            fileList.append(os.path.join(root, name))
+
+
+
+#For each one, add them to the list IF they're encrypted
+#Take each page from the decrypted file and store it
+#Write it to new, unencrypted file
