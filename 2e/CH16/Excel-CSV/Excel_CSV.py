017531b99985f44adb42b9b4782793dafdefb4e7
Clockknight <tylerf2wang@gmail.com>
2020-02-28 17:57:44 -0800

    Update Excel_CSV.py
    Quick update, need to write to csv file

--- 2e/CH16/Excel-CSV/Excel_CSV.py
+++ 2e/CH16/Excel-CSV/Excel_CSV.py
@@ -5,22 +5,40 @@ import openpyxl
 from openpyxl import workbook
 
 def main():
-    # 1) Read all excel sheets in CWD
-    for root, dir, file in os.walk(os.getcwd(), topdown = False):
+    for root, dir, file in os.walk(os.getcwd(), topdown = False):#Check for all files in CWD
         for fileName in file:
-            if fileName[-5:] == '.xlsx':
-                convert(root, fileName)
+            if fileName[-5:] == '.xlsx':#If the file is a .xlsx file, run the convert method on it
+                convert(root, fileName)#Pass the whole file name through, and the root path
 
 
-def convert(root, excelName):
-    fileRootName = excelName[:-5]
-    csvName = fileRootName + '.csv'
-    output = os.path.join(root, csvName)
 
-    # 2) Output all as csv files
+def convert(root, fileTitle):
+    source = os.path.join(root, fileTitle)#The source/xlsx file is here, at root\\fileTitle
+    csvBase = fileTitle[-5:]#csvBase is the name of the file with no extensions
+
+    book = openpyxl.load_workbook(source)#Begin declaring excel related variables
+    sheets = len(book.sheetnames)#Find the amount of sheets in the workbook
+
+    for a in range(0, sheets):
+        sheet = book.worksheets[a]#Get information of the sheet as the program iterates through
+        sheetName = book.sheetnames[a]
+        sheetWidth = sheet.max_column#Find the dimensions of the sheet
+        sheetHeight = sheet.max_row
+
+        csvName = csvBase + sheetName + '.csv'
+        output = os.path.join(root, csvName)
+        target = open()
+
+        for y in range(0, sheetHeight):
+            rowList = []#Clear the rowList variable before moving onto the next row
+
+            for x in range(0, sheetWidth):
+                    rowList[x] = sheet.cell(row=y, column=x).value#Gather values from the sheet's row
 
 
-    # 3) 1 csv file per excel sheet
-    # 4) Titled <filename>_<sheetname>.csv
+
+        # 3) 1 csv file per excel sheet
+
+
 
 main()
