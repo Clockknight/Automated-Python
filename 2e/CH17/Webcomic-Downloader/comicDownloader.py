1643d40e725d23297eef5cf34f29ad961a2765b4
Clockknight <tylerf2wang@gmail.com>
2020-03-26 21:32:09 -0700

    Layout prepared for main function, and

--- 2e/CH17/Webcomic-Downloader/comicDownloader.py
+++ 2e/CH17/Webcomic-Downloader/comicDownloader.py
@@ -2,13 +2,13 @@ import os
 import bs4
 import requests
 
-inclusiveBool = True
+inclusiveInt = 0
 file = './target.txt'
 infoArray = []
 
 os.makedirs('Order Of the Stick', exist_ok=True)
 
-def downloadPage(inputSoup):
+def downloadPage(inputSoup, iterCount):
 
     imageList = inputSoup.select('img')
     sourceList = []
@@ -21,48 +21,68 @@ def downloadPage(inputSoup):
             comicUrl = source[36:]
 
             #Save the image to ./Order Of The Stick.
-            print('Downloading ' + comicUrl + '...')
+            #Only
+            if iterCount > 25:
+                print('Downloading ' + comicUrl + '...')
+                iterCount = 0
+
             res = requests.get(source)
             imageFile = open(os.path.join('Order Of the Stick', os.path.basename(comicUrl)), 'wb')
             for chunk in res.iter_content(100000):
                 imageFile.write(chunk)
             imageFile.close()
 
-            return 0
+            return iterCount
 
 #Self iterating function to download all pages after initial Page
-def downloadNext(inputSoup):
+def downloadNext(inputSoup, iterCount):
 
-    downloadPage(inputSoup)
+    #Keep track of the iterations that have occured
+    iterCount += 1
+    confirmedHref = ''
+
+    #Download the comic
+    downloadPage(inputSoup, iterCount)
+
 
     #Find 'next button'
         #Next comic button has "Next Comic" for a title
+        #Find all a tags with img tags
+        #Select all img tags within
+        #Select each img tag
+        #Find the one with the next comic title
+        #Find the matching a
+        #Pull that a tag's href
+        pageLink = confirmedHref
 
     #Find where it's directing to
 
     #make that the target of nextSoup soup object
     res = requests.get(nextPage)
 
-    #downloadNext(nextSoup)
-
-    return downloadNext(nextSoup)
+    if nextSoup == inputSoup:
+        return pageLink
+    else:
+        return downloadNext(nextSoup, iterCount)
 
 #VVVV Main codeblock starts HERE VVVV
+#Codeblock for Initializing files, links, and variables that depend on those links
 #Pull a target url if there are any, from inside the file
 target = open(file, 'r+')
-infoArray = target.readlines()
+targetInfo = target.read()
 target.close()
 
 #If the info array pulls no text, then assume default url
-if len(infoArray) == 0:
+if targetInfo == '':
     print('No link found in target.txt. Taking default url.')
     url = 'https://www.giantitp.com/comics/oots0001.html'
 
 else:
     #If there is a VALID link in the file, the code will take that and will not download that same page again.
-    if infoArray[0][:36] == 'https://www.giantitp.com/comics/oots':
+    if targetInfo[:36] == 'https://www.giantitp.com/comics/oots':
         print('Link found in target.txt. Using that url to download pages.')
-        inclusiveBool = False
+        #If a link is found, it means that page has already been read, so that comic will not be downloaded
+        inclusiveInt = -1
         url = infoArray[0]
 
     #Otherwise, the code will assume the default url.
@@ -72,16 +92,20 @@ else:
 
 print('Link obtained. It points to: ' + url)
 
+#Codeblock to actually begin the code
 #Go to URL
 res = requests.get(url)
 
+#Obtain current page's soup
 try:
     res.raise_for_status()
     initialSoup = bs4.BeautifulSoup(res.text, 'html.parser')
 
-    if inclusiveBool:
-        downloadPage(initialSoup)
+    #Download current page's comic
+    url = downloadNext(initialSoup, 0)
 
+
+#Exception clause
 except Exception as exc:
     print('There was problem: %s' % (exc))
 
