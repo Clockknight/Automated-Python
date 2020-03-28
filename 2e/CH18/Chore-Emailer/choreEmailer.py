d0930d6438ef79f9712499e8bf3a6e19f38f2fab
Clockknight <tylerf2wang@gmail.com>
2020-03-28 00:09:47 -0700

    Set up rest of book's file architecture

---
+++ 2e/CH18/Chore-Emailer/choreEmailer.py
@@ -0,0 +1,9 @@
+
+
+chores = ['Bathroom', 'Kitchen', 'Bedroom', 'Living Room']
+recipientList = []
+
+#Take inputs for emails
+#Validate each email
+#If valid, append to recipientList
+#When BEGIN is input, start the program
